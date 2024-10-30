f12590e6473091e3457a01c053c57acaa13b422f
Barbarayoussef <barayoussef12@yahoo.com>
2024-10-30 15:32:29 -0400

    - Show only Button Fragment if portrait and switch fragments if portrait

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 
 
 /*
@@ -19,18 +20,40 @@ The Activity layout files for both Portrait and Landscape are already provided
 */
 
 class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
+
+    lateinit var dieViewModel: DieViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+           dieViewModel=ViewModelProvider(this)[DieViewModel::class.java]
 
         /* TODO 1: Load fragment(s)
             - Show only Button Fragment if portrait
             - show both fragments if Landscape
           */
+        if(savedInstanceState==null)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.container1,ButtonFragment())
+            .addToBackStack(null)
+            .setReorderingAllowed(true)
+            .commit()
+        dieViewModel.getDieRoll().observe(this){
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container1,DieFragment())
+                .addToBackStack(null)
+                .setReorderingAllowed(true)
+                .commit()
+        }
+
+        if()
     }
 
     /* TODO 2: switch fragments if portrait (no need to switch fragments if Landscape)
         */
+
     // Remember to place Fragment transactions on BackStack so then can be reversed
     override fun buttonClicked() {
 
